f1b19bc900142ed97276103b15dda071f3b66810
Andrea Stagi <stagi.andrea@gmail.com>
2014-06-25 03:15:26 +0200

    Create manager function

--- osdescriptors/__init__.py
+++ osdescriptors/__init__.py
@@ -1,11 +1,15 @@
-from openelectv import OpenElecTV
-from raspbian import Raspbian
 from raspberry import Raspberry
 
+def create_manager(name, id=None):
+  if not id:
+    id = name
+  return type(name, (Raspberry,), {'id':id})()
+
 register = []
-register.append(type('Raspbian', (Raspberry,), {'id':'Raspbian'})())
-register.append(type('Pidora', (Raspberry,), {'id':'Pidora'})())
-register.append(type('OpenElecTV', (Raspberry,), {'id':'OpenELEC'})())
-register.append(type('RaspBMC', (Raspberry,), {'id':'RaspBMC'})())
-register.append(type('RISC OS', (Raspberry,), {'id':'RISC OS'})())
-register.append(type('ARCH LINUX', (Raspberry,), {'id':'Raspbian'})())
+register.append(create_manager('Raspbian'))
+register.append(create_manager('Pidora'))
+register.append(create_manager('OpenElecTV', 'OpenElec'))
+register.append(create_manager('RaspBMC'))
+register.append(create_manager('RISC OS'))
+register.append(create_manager('ARCH LINUX'))
+
